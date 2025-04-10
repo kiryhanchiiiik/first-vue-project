c670c50b02e89f2481605d72a13b1768b7d16854
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-04-10 17:02:41 +0300

    add logic

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,12 +1,21 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
+import { ref } from 'vue'
+
+const buttonVariant = ref('gradient')
+
+const changeButtonVariant = () => {
+  buttonVariant.value = buttonVariant.value === 'gradient' ? 'outlined' : 'gradient'
+}
 </script>
 
 <template>
   <div class="px-6">
     <div class="text-gray mb-4">Додані маркери</div>
     <FavoritePlace :key="n" v-for="n in 4" />
-    <IButton class="w-full mt-10">Додати маркер</IButton>
+    <IButton :variant="buttonVariant" class="w-full mt-10" @click="changeButtonVariant"
+      >Додати маркер</IButton
+    >
   </div>
 </template>
--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,5 +1,27 @@
+<script setup>
+import { defineProps, computed, toRefs } from 'vue'
+
+const props = defineProps({
+  variant: {
+    default: 'primary',
+    type: String,
+    validator: (value) => {
+      return ['primary', 'gradient', 'outlined'].includes[value]
+    }
+  }
+})
+
+const { variant } = toRefs(props)
+
+const bgStyles = computed(() => {
+  return variant.value === 'gradient'
+    ? 'bg-gradient-to-r from-[#FFA279] to-[#F3743D]'
+    : 'bg-[#FFA279]'
+})
+</script>
+
 <template>
-  <button class="bg-[#FFA279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider">
+  <button class="rounded-xl py-3 px-10 text-white font-bold -tracking-wider" :class="bgStyles">
     <slot></slot>
   </button>
 </template>
